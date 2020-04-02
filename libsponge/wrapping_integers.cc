add3e768b33af6e553ba7f721af23f22b199098a
Ofey Chan <ofey206@gmail.com>
2020-04-01 22:38:31 -0400

    add comments to clearify the code

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,7 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    return isn + uint32_t(n);
+    return isn + uint32_t(n);  // This plus operator between type is overloaded.
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,16 +29,21 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     int32_t increment = n - isn;  // could be negative.
+
+    // Generate first test of absolute sequence number.
+    // The first test is set in [base, base+(1ul << 32) )
     uint64_t absoluteIncrement = increment >= 0? increment : (1ul << 32) + increment;
-    uint64_t base = checkpoint & ~0xffffffffUL;
+    uint64_t base = checkpoint & ~0xffffffffUL;  // Higher 32 bit of checkpoint.
     uint64_t test1 = base + absoluteIncrement;
 
+    // If the range between first test and checkpoint is further than half of 
+    // the wrapping interval, the first test would be rejected.
     if ((test1 > checkpoint ? test1 - checkpoint : checkpoint - test1) <= (1ul << 31)) {
         return test1;
-    } else if (test1 < checkpoint) {
+    } else if (test1 < checkpoint) {  // If test1 is smaller, the round up could be done safely.
         return test1 + (1ul << 32);
-    } else { // absolute index can't less than zero.
-        if (base == 0) {
+    } else {
+        if (base == 0) { // Absolute index can't less than zero, so just let it go.
             return test1;
         } else {
             return test1 - (1ul << 32);
